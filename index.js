06c9dc32c392fb34c71a7b40aded3d97e9b0f935
Gabbie Piraino <pirainogi@gmail.com>
2018-10-21 21:30:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function logWhisper(string)
 function sayHiToGrandma(string)
 {
   if(string.toLowerCase() === string) {
-    return "I can't hear you!"
+    return "I can't hear you!";
   }
   else if((string) === uppercase){
     return "YES INDEED!"
